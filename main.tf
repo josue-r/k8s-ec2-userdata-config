cea37d485a6ef265036300d47b0d416fe11fe645
Josue-RS <josue.ruiz@wizeline.com>
2025-07-23 15:30:36 -0600

    test 2 tf plan bot

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ module "worker1" {
   user_data_path     = "${path.module}/user_data/worker.sh"
 }
 
-# module "worker2" {
-#   source             = "./modules/ec2"
-#   name               = "k8s-worker-2"
-#   ami_id             = "ami-020cba7c55df1f615"
-#   instance_type      = "t3.micro"
-#   subnet_id          = aws_subnet.public[2].id
-#   security_group_ids = [aws_security_group.k8s.id]
-#   key_name           = "test_key_demo"
-#   user_data_path     = "${path.module}/user_data/worker.sh"
-# }
+module "worker2" {
+  source             = "./modules/ec2"
+  name               = "k8s-worker-2"
+  ami_id             = "ami-020cba7c55df1f615"
+  instance_type      = "t3.micro"
+  subnet_id          = aws_subnet.public[2].id
+  security_group_ids = [aws_security_group.k8s.id]
+  key_name           = "test_key_demo"
+  user_data_path     = "${path.module}/user_data/worker.sh"
+}
