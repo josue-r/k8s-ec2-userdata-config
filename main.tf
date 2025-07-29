cff4bbe42d1e8f99323e3d11c1471529cd9c617e
Josue-RS <josue.ruiz@wizeline.com>
2025-07-28 18:18:26 -0600

    chore(ec2):worker node set up, instance profile added

--- main.tf
+++ main.tf
@@ -15,30 +15,32 @@ module "master" {
 }
 
 module "worker1" {
-  source             = "./modules/ec2"
-  enabled            = true
-  name               = "k8s-worker-1"
-  ami_id             = "ami-020cba7c55df1f615"
-  instance_type      = "t3.small"
-  subnet_id          = aws_subnet.public[1].id
-  security_group_ids = [aws_security_group.k8s.id]
-  key_name           = "test_key_demo"
-  user_data_path     = "${path.module}/user_data/worker.sh"
+  source               = "./modules/ec2"
+  enabled              = true
+  name                 = "k8s-worker-1"
+  ami_id               = "ami-020cba7c55df1f615"
+  instance_type        = "t3.small"
+  subnet_id            = aws_subnet.public[1].id
+  security_group_ids   = [aws_security_group.k8s.id]
+  key_name             = "test_key_demo"
+  user_data_path       = "${path.module}/user_data/worker.sh"
+  iam_instance_profile = module.worker_node_iam.iam_instance_profile_name
   extra_tags = {
     Role = "worker-node"
   }
 }
 
 module "worker2" {
-  source             = "./modules/ec2"
-  enabled            = true
-  name               = "k8s-worker-2"
-  ami_id             = "ami-020cba7c55df1f615"
-  instance_type      = "t3.small"
-  subnet_id          = aws_subnet.public[2].id
-  security_group_ids = [aws_security_group.k8s.id]
-  key_name           = "test_key_demo"
-  user_data_path     = "${path.module}/user_data/worker.sh"
+  source               = "./modules/ec2"
+  enabled              = true
+  name                 = "k8s-worker-2"
+  ami_id               = "ami-020cba7c55df1f615"
+  instance_type        = "t3.small"
+  subnet_id            = aws_subnet.public[2].id
+  security_group_ids   = [aws_security_group.k8s.id]
+  key_name             = "test_key_demo"
+  user_data_path       = "${path.module}/user_data/worker.sh"
+  iam_instance_profile = module.worker_node_iam.iam_instance_profile_name
   extra_tags = {
     Role = "worker-node"
   }
